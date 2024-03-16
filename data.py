4f6b9d8c97a1b4269d627d56d1c44fee17351e1b
Amd A9 <sok235u@gmail.com>
2024-03-16 10:23:21 +0900

    init comit

---
+++ data.py
@@ -0,0 +1,13 @@
+order_body = {
+    "firstName": "Артем",
+    "lastName": "Соколов",
+    "address": "Малая Бронная, 56",
+    "metroStation": 98,
+    "phone": "+79245837938",
+    "rentTime": 2,
+    "deliveryDate": "2024-03-25",
+    "comment": "Будет весело",
+    "color": [
+        "BLACK"
+    ]
+}
